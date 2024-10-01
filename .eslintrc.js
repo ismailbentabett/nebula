c8efbf354bcf814dbd2a315189d234eba8d2d643
Ismail Bentabet <ismailbentabett@gmail.com>
2024-10-01 18:16:00 +0100

    Add ESLint configuration file

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint', 'prettier'],
+    extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  };  
